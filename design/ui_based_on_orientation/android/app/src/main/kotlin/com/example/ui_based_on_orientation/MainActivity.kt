c86c215a181d89ded746c5198ac926dc3cccfd02
vbossard <vbossard@symalen.com>
2021-04-07 12:03:53 +0200

    Orientation OK

---
+++ design/ui_based_on_orientation/android/app/src/main/kotlin/com/example/ui_based_on_orientation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ui_based_on_orientation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ design/ui_based_on_orientation/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Design Orientation'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: OrientationBuilder(
+          builder: (context, orientation) {
+            return GridView.count(
+              crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
+              children: List.generate(100, (index) {
+                return Center(
+                    child: Text('Item $index',
+                        style: Theme.of(context).textTheme.headline1));
+              }),
+            );
+        }));
+  }
+}
