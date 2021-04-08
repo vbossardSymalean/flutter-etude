1b0f8e24a612dd3c911e65173bf6ddb545f58f70
vbossard <vbossard@symalen.com>
2021-04-08 11:03:51 +0200

    Retrieve Field. Unfinished cause State Problem

---
+++ forms/retrieve_field_value/android/app/src/main/kotlin/com/example/retrieve_field_value/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.retrieve_field_value
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ forms/retrieve_field_value/lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Formulaire',
+            ),
+            MyCustomForm()
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          return showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  content: Text('Bonjour'),
+                );
+              });
+        },
+      ),
+    );
+  }
+}
+
+typedef void StringCallback(String text);
+
+// Define a custom Form widget.
+class MyCustomForm extends StatefulWidget {
+  
+
+  @override
+  _MyCustomFormState createState() => _MyCustomFormState();
+}
+
+// Define a corresponding State class.
+// This class holds the data related to the Form.
+class _MyCustomFormState extends State<MyCustomForm> {
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final myController = TextEditingController();
+  final StringCallback onTextChanged;
+  
+  
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.all(15.0),
+        child: TextFormField(
+          controller: myController,
+          onChanged: (myController) {
+            onTextChange(myController.value);
+          },
+        ));
+  }
+}
